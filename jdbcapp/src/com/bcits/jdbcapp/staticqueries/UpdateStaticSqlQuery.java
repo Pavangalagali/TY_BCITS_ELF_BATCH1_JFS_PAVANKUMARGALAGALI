5a0c80336e8c6f01e1eabe01c487a45520f2e613
pavankumar galagali <pavankumargalagali@gmail.com>
2020-01-02 16:11:16 +0530

    html

--- jdbcapp/src/com/bcits/jdbcapp/staticqueries/UpdateStaticSqlQuery.java
+++ jdbcapp/src/com/bcits/jdbcapp/staticqueries/UpdateStaticSqlQuery.java
@@ -15,35 +15,35 @@ public class UpdateStaticSqlQuery {
 			FileInputStream file = new FileInputStream("dbInfo.properties");
 			Properties properties = new Properties();
 			properties.load(file);
-			
-			//step 1 : load the driver 
+
+			// step 1 : load the driver
 			Class.forName(properties.getProperty("drivername")).newInstance();
-			
-			//step 2 : get the bd connection via driver
+
+			// step 2 : get the bd connection via driver
 			connection = DriverManager.getConnection(properties.getProperty("dbUrl"), properties);
-			
-			//step 3 : issue the sql query
+
+			// step 3 : issue the sql query
 			stmt = connection.createStatement();
 			int result = stmt.executeUpdate(properties.getProperty("query1"));
-			
-			//step 4 : process the result returned by query
-			System.out.println(result +" rows affected");
+
+			// step 4 : process the result returned by query
+			System.out.println(result + " rows affected");
 		} catch (Exception e) {
-		
+
 			e.printStackTrace();
-		}finally {
+		} finally {
 			try {
-				if(connection!=null) {
+				if (connection != null) {
 					connection.close();
 				}
-				if(stmt!=null) {
+				if (stmt != null) {
 					stmt.close();
 				}
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
+
 				e.printStackTrace();
 			}
 		}
 	}
-	
+
 }
