31b460683d05fa6f85a64ca1d6fc30fc91104350
pavankumar galagali <pavankumargalagali@gmail.com>
2020-01-24 18:36:21 +0530

    spring mvc

--- springcore/src/main/java/com/bcits/springcore/EmployeeTest2.java
+++ springcore/src/main/java/com/bcits/springcore/EmployeeTest2.java
@@ -7,12 +7,12 @@ import com.bcits.springcore.beans.EmployeeBean;
 
 public class EmployeeTest2 {
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("employeeConfig.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("importAllConfigs.xml");
 		EmployeeBean employeeBean =  context.getBean("employee", EmployeeBean.class);
 	
-		System.out.println("Name : "+employeeBean.getName());
-		System.out.println("Age  : "+employeeBean.getAge());
-		System.out.println("Dept Id : "+employeeBean.getDepartmentBean().getDeptId());
+		System.out.println("     Name : "+employeeBean.getName());
+		System.out.println("      Age : "+employeeBean.getAge());
+		System.out.println("Dept Id   : "+employeeBean.getDepartmentBean().getDeptId());
 		System.out.println("Dept Name : "+employeeBean.getDepartmentBean().getDeptName());
 	}
 }
